13928a56f4bdb0c09773ce7e9b2eb33df8d81f6f
Ruslan Mazavin <ruslan.mazavin@gmail.com>
2021-12-10 15:00:10 +0300

    Tap tempo works at last!

--- lib/taptempo/taptempo.cpp
+++ lib/taptempo/taptempo.cpp
@@ -12,7 +12,7 @@ TapTempo::TapTempo(uint8_t pin, long minBpm, long maxBpm, uint8_t tapPolarity): 
     }
 
 long TapTempo::calculateIntervalMicroSecs(long _bpm) {          // Take care about overflows!
-//return 60L * 1000 * 1000 * 10 / _bpm / CLOCKS_PER_BEAT;        // ok
+//return 60L * 1000 * 1000 * 10 / _bpm / CLOCKS_PER_BEAT;       // ok
 return 600000000/(24*_bpm);
 }
 
@@ -23,7 +23,7 @@ bool TapTempo::check(){                                           /* Handle tapp
   switch (buttonState)
   {
     case MD_UISwitch::KEY_NULL: {
-      isHeld = false;
+      //isHeld = false;
       //Serial.println("KEY NULL");
       return false;
       break;
@@ -36,17 +36,17 @@ bool TapTempo::check(){                                           /* Handle tapp
       return false;
       break;
     }
-    case MD_UISwitch::KEY_DOWN: {                                 // At last, a keypress
+        case MD_UISwitch::KEY_DOWN: {                                 // At last, a keypress
     Serial.println("KEY DOWN");
     isHeld = true;
     Serial.println("ISHELD!");
     long now = micros();
   if (now - lastTapTime < minimalTapInterval) {
       Serial.println("DEBOUNCE!");
-      Serial.print("now - lastTapTime =");
+      /*Serial.print("now - lastTapTime =");
       Serial.println(now - lastTapTime );
       Serial.print("minimalTapInterval =");  
-      Serial.println(minimalTapInterval); 
+      Serial.println(minimalTapInterval); */
     return false;     // Debounce
     break;
   }
@@ -72,25 +72,25 @@ bool TapTempo::check(){                                           /* Handle tapp
     // Single taps, not enough to calculate a BPM -> ignore!
     //    Serial.println("Ignoring lone taps!");
     Serial.println("SINGLE tap!");
-      Serial.print("now - lastTapTime =");
+      /*Serial.print("now - lastTapTime =");
       Serial.println(now - lastTapTime );
       Serial.print("maximumTapInterval =");  
-      Serial.println(maximumTapInterval); 
+      Serial.println(maximumTapInterval); */
       timesTapped = 0;
-      //lastTapTime = now;
+
       return false;
   } else if (timesTapped >= MINIMUM_TAPS) {
                     Serial.println("Enough taps!");
-                    Serial.print("now - lastTapTime =");
+                   /* Serial.print("now - lastTapTime =");
                     Serial.println(now - lastTapTime );
                     Serial.print("maximumTapInterval =");  
-                    Serial.println(maximumTapInterval); 
+                    Serial.println(maximumTapInterval); */
     long avgTapInterval = (lastTapTime - firstTapTime) / (timesTapped - 1);
-                    Serial.print("avgTapInterval =");  
-                    Serial.println(avgTapInterval); 
+                    /*Serial.print("avgTapInterval =");  
+                    Serial.println(avgTapInterval); */
     if ((now - lastTapTime) > (avgTapInterval * EXIT_MARGIN / 100)) {
-                          Serial.print("asderval =");  
-                    Serial.println(avgTapInterval * EXIT_MARGIN / 100); 
+                          /*Serial.print("asderval =");  
+                    Serial.println(avgTapInterval * EXIT_MARGIN / 100); */
                   Serial.println("Good timing!!");
       newTempo = ((60L * 1000 * 1000 * 5 / avgTapInterval)/10)*10;
       lastTapTime = now;
@@ -98,86 +98,14 @@ bool TapTempo::check(){                                           /* Handle tapp
       return true;
       }
       timesTapped = 0;
-      //lastTapTime = now;
       return false;
   }
-    /*
-      if ((now - lastTapTime) < minimalTapInterval){                // debounce
-        return false;
-      }
 
-      if (timesTapped == 0) {
-      firstTapTime = now;
-      lastTapTime = now;
-      timesTapped++;
-      return false;
-      }
-      
-      //lastTapTime = now;
-      //isHeld = true;
-      
-      if (timesTapped > 0 && timesTapped < MINIMUM_TAPS && (now - lastTapTime) > maximumTapInterval) {  // Single taps, not enough to calculate BPM -> ignore!
-                                                           
-        isHeld = true;   
-        Serial.print("Single tap,  timesTapped =");  
-        Serial.println(timesTapped); 
-        Serial.print("now =");
-        Serial.println(now );
-        Serial.print("lastTapTime =");
-        Serial.println(lastTapTime );
-        Serial.print("now - lastTapTime =");
-        Serial.println(now - lastTapTime );
-        Serial.print("maximumTapInterval =");  
-        Serial.println(maximumTapInterval); 
-        Serial.print("minimalTapInterval =");  
-        Serial.println(minimalTapInterval); 
-        timesTapped = 0;
-        lastTapTime = now;
-        return false;
-        break;
-        }
-        
-        if (timesTapped >= MINIMUM_TAPS) {                              // enough taps for calculating a new tempo
-          avgTapInterval = (lastTapTime - firstTapTime) / (timesTapped - 1);
-          Serial.print("Enough taps,  timesTapped =");  
-          Serial.println(timesTapped); 
-          Serial.print("now =");
-          Serial.println(now );
-          Serial.print("lastTapTime =");
-          Serial.println(lastTapTime );
-          Serial.print("firstTaptime =");
-          Serial.println(firstTapTime );
-          Serial.print("now - lastTapTime =");
-          Serial.println(now - lastTapTime );
-          Serial.print("avgTapInterval =");  
-          Serial.println(avgTapInterval); 
-          Serial.print("maximumTapInterval =");  
-          Serial.println(maximumTapInterval); 
-          Serial.print("avgTapInterval * EXIT_MARGIN / 100 =");  
-          Serial.println(avgTapInterval * EXIT_MARGIN / 100); 
-        
-          timesTapped = 0;
-          if ((now - lastTapTime) > (avgTapInterval * EXIT_MARGIN / 100)) {    // If taps are consistent enough
-          newTempo = 60L * 1000 * 1000 * 10 / avgTapInterval;               // Convert interval to tempo in BPM
-          //newTempo = 800;
-          isHeld = true; 
-          lastTapTime = now;                                                     //
-          return true;
-        } else{
-          timesTapped = 0;                                                  // Reset taps counter
-          isHeld = true;
-          lastTapTime = now;
-          return false;
-        }
-        lastTapTime = now;
-        }                                                            // not enough taps
-        return false;  
-        
-      */
-      //lastTapTime = now;
     break;
     }
-    default: break;
+    default: {
+    return false;
+    break;}
   }
   return false;
-};
+}
